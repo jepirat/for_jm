23f790e8bdb124859afcfc7e9c3b71529f83f0c4
Евгений <74641248+jepirat@users.noreply.github.com>
2021-06-20 17:12:53 +0400

    Добавил запрос на добавление таблмцы

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,29 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
+        try (Connection connection = Util.connect()) {
+            StringBuilder QUERY = new StringBuilder();
+            QUERY.append("CREATE TABLE IF NOT EXISTS Users(");
+            QUERY.append("id MEDIUM NOT NULL AUTO_INCREMENT PRIMARY KEY, ");
+            QUERY.append("name text NOT NULL, ");
+            QUERY.append("lastName text NOT NULL, ");
+            QUERY.append("age int NOT NULL)");
+            PreparedStatement statement = connection.prepareStatement(QUERY.toString());
+            statement.executeQuery();
+        } catch (SQLException e) {
 
+        }
     }
 
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    public Connection connect() throws SQLException {
+    public static Connection connect() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "feniks");
     }
 }
