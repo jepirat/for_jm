831ebb9370680e5c090a45636c328f0eee7a5418
Евгений <74641248+jepirat@users.noreply.github.com>
2021-06-23 15:36:37 +0400

    Вроде бы все
    ыыфы

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+       UserServiceImpl userService = new UserServiceImpl();
+       userService.createUsersTable();
+       userService.saveUser("jeka", "pettov", (byte) 35);
+       userService.saveUser("petya", "sidorov", (byte) 38);
+       userService.saveUser("vasja", "turkon", (byte) 45);
+       userService.saveUser("gena", "popovich", (byte) 59);
+       List<User> allUsers = userService.getAllUsers();
+       allUsers.forEach(System.out::println);
+       userService.cleanUsersTable();
+       userService.dropUsersTable();
     }
 }
