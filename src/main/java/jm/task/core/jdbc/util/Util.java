216d2a77eaacfa81f550960a10c58a514a914d8a
Еvgeniy <evkafanov@gmail.com>
2021-06-25 01:07:46 +0400

    Добавил зависимости

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.Hibernate;
+import org.hibernate.hql.internal.ast.util.SessionFactoryHelper;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     public Connection connect() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "feniks");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "jekasql", "12345");
+    }
+
+    public void hiber() {
+
     }
 }
