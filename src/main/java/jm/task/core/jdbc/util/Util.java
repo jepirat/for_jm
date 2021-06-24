9cf701a9cba716197dffb5d29feba72237e2cb25
Еvgeniy <evkafanov@gmail.com>
2021-06-24 23:02:58 +0400

    изменил настрйки подключения

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,6 +6,6 @@ import java.sql.SQLException;
 
 public class Util {
     public static Connection connect() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "feniks");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "jekasql", "12345");
     }
 }
