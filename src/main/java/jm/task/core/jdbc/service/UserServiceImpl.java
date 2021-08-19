b2e4ea106d28638a64f2b153b582c7bbada80d48
jeka <evgeshakev@gmail.com>
2021-08-19 19:33:00 +0400

    да то было не правильно

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,28 +7,29 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
     public void createUsersTable() throws SQLException {
-        new UserDaoHibernateImpl().createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() throws SQLException {
-        new UserDaoHibernateImpl().dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        new UserDaoHibernateImpl().saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
         System.out.println("User с именем - " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
-       new UserDaoHibernateImpl().removeUserById(id);
+        userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return new UserDaoHibernateImpl().getAllUsers();
+        return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        new UserDaoHibernateImpl().cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
     }
 }
